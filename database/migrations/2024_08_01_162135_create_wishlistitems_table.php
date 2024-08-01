0c17fcca1d7d347faa770734a86e7c19734d9af7
Shivangikoradiya <shivangikoradiya9@gmail.com>
2024-08-01 11:22:54 -0500

    created wishlist and whishlist items table file in migration

---
+++ database/migrations/2024_08_01_162135_create_wishlistitems_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wishlistitems', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('wishlistid')->unsigned()->nullable();
+            $table->bigInteger('productid')->unsigned()->nullable();
+
+            // Define the foreign keys
+            $table->foreign('wishlistid')->references('id')->on('wishlist')->onDelete('set null');
+            $table->foreign('productid')->references('id')->on('products')->onDelete('set null');
+
+            // Add indexes to the foreign key columns
+            $table->index('wishlistid');
+            $table->index('productid');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wishlistitems');
+    }
+};
