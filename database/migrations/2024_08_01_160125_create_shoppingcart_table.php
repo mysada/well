6100f9db5ee52ab5eac1dc9465da73b8f3cc1771
Shivangikoradiya <shivangikoradiya9@gmail.com>
2024-08-01 11:03:36 -0500

    created shoppincart table file in migration

---
+++ database/migrations/2024_08_01_160125_create_shoppingcart_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shoppingcart', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('userid')->unsigned()->nullable();
+            $table->dateTime('createddate')->default(DB::raw('CURRENT_TIMESTAMP'));
+
+            // Define the foreign key
+            $table->foreign('userid')->references('id')->on('users')->onDelete('set null');
+
+            // Add an index to the userid column
+            $table->index('userid');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shoppingcart');
+    }
+};
