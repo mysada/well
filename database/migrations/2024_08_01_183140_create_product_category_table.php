896aa1ee607e54f338a32ffdae9bcfd0452b1cfe
Shivangikoradiya <shivangikoradiya9@gmail.com>
2024-08-01 13:33:01 -0500

    added product_category table

---
+++ database/migrations/2024_08_01_183140_create_product_category_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_category', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('product_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+
+            // Define the foreign keys
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_category');
+    }
+};
