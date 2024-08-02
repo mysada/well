f6e3545aaa232f83fd036ee69df2bb9ef9cff3b7
Arthur NightFae <paracidex@gmail.com>
2024-08-02 15:37:47 -0500

    feat(route): cart route, controller, and blade structure

--- app/Http/Controllers/well/CartItemController.php
+++ app/Http/Controllers/well/CartItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\well;
 
+use App\Models\CartItem;
 use Illuminate\Http\Request;
 
 class CartItemController extends Controller
@@ -12,7 +13,10 @@ class CartItemController extends Controller
      */
     public function index()
     {
-        //
+        $carts = CartItem::all();
+        $title = 'Shopping Cart';
+
+        return view('well.pages.cart', compact('carts', 'title'));
     }
 
     /**
