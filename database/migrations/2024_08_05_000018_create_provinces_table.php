a74429c68861c1a1f6d79dccd0effe71973867b8
Arthur NightFae <paracidex@gmail.com>
2024-08-04 19:09:42 -0500

    feat(db): provinces migration and seeder

---
+++ database/migrations/2024_08_05_000018_create_provinces_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->id();
+            $table->string('country_code',10);
+            $table->string('name',255);
+            $table->string('short_name',255);
+            $table->decimal('gst_rate',4,2);
+            $table->decimal('pst_rate',4,2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('provinces');
+    }
+};
