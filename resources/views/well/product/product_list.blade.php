aeceea3082d55c7cebc744656c99e3d021cb619e
manish kumar <manishkumar@breezyloans.net.au>
2024-08-06 13:09:13 -0500

    Product Controller Updated, Image Url Changes in Seeder file, Product List Blade Template updated

--- resources/views/well/product/product_list.blade.php
+++ resources/views/well/product/product_list.blade.php
@@ -12,39 +12,32 @@
         <div class="row">
             <div class="col-md-3">
                 <div class="category-list">
-                    <a href="#" class="active-category">Best Seller</a>
-                    <a href="#">Skincare</a>
-                    <a href="#">Fitness</a>
-                    <a href="#">Supplements</a>
+                    @foreach ($categories as $category)
+                    <a href="{{ route('products.index', ['category_id' => $category->id]) }}"
+                       class="{{ isset($category_id) && $category_id == $category->id ? 'active-category' : '' }}">
+                        {{ htmlspecialchars($category->name) }}
+                    </a>
+                    @endforeach
                 </div>
             </div>
             <div class="col-md-9">
                 <div class="row">
-                    @for ($i = 1; $i <= 9; $i++)
-                        <div class="col-md-4 mb-4">
-                            <div class="card product-card h-100">
-                                <img src="{{ asset("images/list_view/list_p$i.jpg") }}" class="card-img-top" alt="Product {{ $i }}">
-                                <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title">Nyantuy Skincare</h5>
-                                    <p class="card-text mt-auto">$ 56</p>
-                                    <button class="btn-add mt-3">+</button>
-                                </div>
+                    @foreach ($products as $product)
+                    <div class="col-md-4 mb-4">
+                        <div class="card product-card h-100">
+                            <img src="{{ asset($product->image_url) }}" class="card-img-top" alt="{{ htmlspecialchars($product->name) }}">
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title">{{ htmlspecialchars($product->name) }}</h5>
+                                <p class="card-text mt-auto">$ {{ number_format($product->price, 2) }}</p>
+                                <a href="{{ route('products.show', $product->id) }}" class="btn-add mt-3">+</a>
                             </div>
                         </div>
-                    @endfor
+                    </div>
+                    @endforeach
                 </div>
                 <div class="text-center">
-                    <button class="btn btn-load-more">Load More</button>
+                    {{ $products->appends(['category_id' => $category_id])->links() }}
                 </div>
-                <nav aria-label="Page navigation" class="mt-4">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                    </ul>
-                </nav>
             </div>
         </div>
     </div>
