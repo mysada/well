e535b7020bea8ff913ce6d0ea4c895e4a6e3add1
Shivangikoradiya <shivangikoradiya9@gmail.com>
2024-08-06 15:35:44 -0500

    changed image URL path, update css

--- app/Http/Controllers/well/ProductController.php
+++ app/Http/Controllers/well/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\well;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use App\Models\Wishlist;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
