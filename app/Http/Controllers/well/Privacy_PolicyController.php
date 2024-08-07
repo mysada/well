b9eaebc957efb454c55cbb7c2456aff8c3afe056
Aman Dawar <amandawar333@gmail.com>
2024-08-07 16:16:57 -0500

    aman - privacy-policy controller created

---
+++ app/Http/Controllers/well/Privacy_PolicyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\well;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Privacy_PolicyController extends Controller
+{
+    //
+}
