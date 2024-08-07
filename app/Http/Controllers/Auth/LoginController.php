561f421c4b34b258b829634c5263c9778b0a588f
dongqing <nights-1001@outlook.com>
2024-08-07 11:07:18 -0500

    log out flash message set up

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -17,11 +16,16 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    protected function authenticated(Request $request, $user)
+    protected function authenticated()
     {
         // Flash message for successful login
         session()->flash('success', 'Login successful! Welcome back.');
-        return redirect()->intended($this->redirectPath());
     }
 
+    protected function loggedOut()
+    {
+        // Flash message for successful logout
+        session()->flash('success', 'Logout successful! See you again soon.');
+        return redirect('/');
+    }
 }
