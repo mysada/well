f2af0e820f5de19080c016e7fb6efc496aa9b886
Arthur NightFae <paracidex@gmail.com>
2024-08-07 11:44:33 -0500

    feat(wishlist): dynamic ui

--- resources/views/well/pages/wishlist.blade.php
+++ resources/views/well/pages/wishlist.blade.php
@@ -5,33 +5,24 @@
     <section class="wishlist-section">
         <div class="container">
             <h1 style="font-size: 50px;">Wishlist</h1>
-            <div class="product-item">
-                <div class="product-info">
-                    <img src="../../../public/images/product.jpg" alt="Product 1">
-                    <div>
-                        <div class="product-title">Skincare</div>
-                        <div class="product-price">$106.00</div>
+            @foreach($wishlists as $wishlist)
+                <div class="product-item">
+                    <div class="product-info">
+                        <img src="{{asset($wishlist->product->image_url)}}" alt="Product 1">
+                        <div>
+                            <div class="product-title">{{$wishlist->product->name}}</div>
+                            <div class="product-price">${{$wishlist->product->price}}</div>
+                        </div>
+                    </div>
+                    <div class="product-actions">
+                        <button class="btn btn-primary">Add to Cart</button>
+                        <!-- <button class="btn btn-danger">Remove</button> -->
+                        <button class="btn btn-remove" style="color: #666; margin-left: 20px;">Remove</button>
                     </div>
                 </div>
-                <div class="product-actions">
-                    <button class="btn btn-primary">Add to Cart</button>
-                    <!-- <button class="btn btn-danger">Remove</button> -->
-                    <button class="btn btn-remove" style="color: #666; margin-left: 20px;">Remove</button>
-                </div>
-            </div>
-            <div class="product-item">
-                <div class="product-info">
-                    <img src="../../../public/images/product1.jpg" alt="Product 2">
-                    <div>
-                        <div class="product-title">Skincare</div>
-                        <div class="product-price">$106.00</div>
-                    </div>
-                </div>
-                <div class="product-actions">
-                    <button class="btn btn-primary">Add to Cart</button>
-                    <button class="btn btn-remove">Remove</button>
-                </div>
-            </div>
+
+            @endforeach
+
         </div>
     </section>
 @endsection
