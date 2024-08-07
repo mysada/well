58254c7d0c372840a10950ed1cf90e85e13a1a9f
Aman Dawar <amandawar333@gmail.com>
2024-08-07 16:20:09 -0500

    aman - returning view to our page

--- app/Http/Controllers/well/Privacy_PolicyController.php
+++ app/Http/Controllers/well/Privacy_PolicyController.php
@@ -7,5 +7,16 @@ use Illuminate\Http\Request;
 
 class Privacy_PolicyController extends Controller
 {
-    //
+    /**
+     * Show the privacy policy page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $title = 'Privacy Policy';
+
+        return view('well.pages.privacy_policy');
+    }
+
 }
