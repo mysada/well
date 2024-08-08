f7242b65e73edae2352b366c2996ad640358f9fd
dongqing <nights-1001@outlook.com>
2024-08-08 10:46:58 -0500

    admin dashboard set up

---
+++ resources/views/admin/pages/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Users</a></li>
+                <li><a href="#">Orders</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    
+</body>
+</html>
