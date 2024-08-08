b21a1030310e747416f23a88dc1f6a54e88a7aff
Aman Dawar <amandawar333@gmail.com>
2024-08-08 15:26:43 -0500

    aman - returning view for cancellation_refunds;

--- app/Http/Controllers/well/CancellationRefundsController.php
+++ app/Http/Controllers/well/CancellationRefundsController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class CancellationRefundsController extends Controller
 {
-    //
+    public function index()
+    {
+        $title = 'Cancellation & Refunds';
+
+        return view('well.pages.cancellation_refunds');
+    }
 }
