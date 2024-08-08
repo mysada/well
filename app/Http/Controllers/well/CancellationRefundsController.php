dd0c915796272f609e9d57b8e10ba07c36f5e653
Aman Dawar <amandawar333@gmail.com>
2024-08-08 15:24:00 -0500

    aman - controller created

---
+++ app/Http/Controllers/well/CancellationRefundsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\well;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CancellationRefundsController extends Controller
+{
+    //
+}
