5f2b8860206f0f490834d718858863334b88ef3b
manish kumar <manishkumar@breezyloans.net.au>
2024-08-08 17:17:38 -0500

    Reviews Styling under Process

--- resources/views/well/product/product_reviews.blade.php
+++ resources/views/well/product/product_reviews.blade.php
@@ -24,38 +24,42 @@
         <div class="row mt-5">
             <div class="col-md-12">
                 <h4>Reviews</h4>
-                @foreach ($product->reviews as $review)
-                <div class="review">
-                    <div class="review-header">
-                        <div class="review-user-info">
-                            <div>
-                                <h5 class="user-name">{{ $review->user->name }}</h5>
-                                <p class="review-date">Posted on {{ $review->created_at->format('M d, Y') }}</p>
+                <div class="row">
+                    @foreach ($product->reviews as $review)
+                    <div class="col-md-4 mb-4">
+                        <div class="review">
+                            <div class="review-header">
+                                <div class="review-user-info">
+                                    <div>
+                                        <h5 class="user-name">{{ $review->user->name }}</h5>
+                                        <p class="review-date">Posted on {{ $review->created_at->format('M d, Y') }}</p>
+                                    </div>
+                                </div>
+                                <div class="review-rating">
+                                    @for ($i = 0; $i < 5; $i++)
+                                    @if ($review->rating > $i)
+                                    <!-- Filled Star SVG -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#f8ce0b" viewBox="0 0 24 24" width="24" height="24">
+                                        <path d="M12 17.27L18.18 21 16.54 14.19 22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.95L5.82 21z"/>
+                                        <path d="M0 0h24v24H0z" fill="none"/>
+                                    </svg>
+                                    @else
+                                    <!-- Unfilled Star SVG -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#ccc" viewBox="0 0 24 24" width="24" height="24">
+                                        <path d="M12 17.27L18.18 21 16.54 14.19 22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.95L5.82 21z" fill="none" stroke="#ccc" stroke-width="2"/>
+                                        <path d="M0 0h24v24H0z" fill="none"/>
+                                    </svg>
+                                    @endif
+                                    @endfor
+                                </div>
+                            </div>
+                            <div class="review-body">
+                                <p class="review-text">{{ $review->review_text }}</p>
                             </div>
                         </div>
-                        <div class="review-rating">
-                            @for ($i = 0; $i < 5; $i++)
-                            @if ($review->rating > $i)
-                            <!-- Filled Star SVG -->
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="#f8ce0b" viewBox="0 0 24 24" width="24" height="24">
-                                <path d="M12 17.27L18.18 21 16.54 14.19 22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.95L5.82 21z"/>
-                                <path d="M0 0h24v24H0z" fill="none"/>
-                            </svg>
-                            @else
-                            <!-- Unfilled Star SVG -->
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="#ccc" viewBox="0 0 24 24" width="24" height="24">
-                                <path d="M12 17.27L18.18 21 16.54 14.19 22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.95L5.82 21z" fill="none" stroke="#ccc" stroke-width="2"/>
-                                <path d="M0 0h24v24H0z" fill="none"/>
-                            </svg>
-                            @endif
-                            @endfor
-                        </div>
                     </div>
-                    <div class="review-body">
-                        <p class="review-text">{{ $review->review_text }}</p>
-                    </div>
+                    @endforeach
                 </div>
-                @endforeach
             </div>
         </div>
 
