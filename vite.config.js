c9245c8c4653bef066274a286982fb527a78fe1f
Arthur NightFae <paracidex@gmail.com>
2024-08-09 13:35:24 -0500

    feat(ui): use glob to import all js and css

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { sync as globSync } from 'glob';
 
+const jsFiles = globSync('resources/js/**/*.js');
+const cssFiles = globSync('resources/sass/**/*.scss');
+
+const adminStyleScssFiles = globSync('resources/admin/style/**/*.scss');
+
+const adminJsFiles = globSync('resources/admin/js/**/*.js');
 
 export default defineConfig({
   plugins: [
     laravel({
       input: [
-        'resources/sass/app.scss',
-        'resources/js/app.js',
-        'resources/js/checkout.js',
-        'resources/js/cart.js',
-        'resources/js/quantity.js',
-        'resources/js/slider-init.js',
-        'resources/js/billing.js',
-        'resources/sass/home.scss',
-        'resources/sass/cart.scss',
-        'resources/sass/product_details.scss',
-        'resources/sass/thank_you.scss',
-        'resources/sass/product_list.scss',
-        'resources/sass/about.scss',
-        'resources/sass/checkout.scss',
-        'resources/sass/wishlist.scss',
-        'resources/sass/privacy_policy.scss',
-        'resources/sass/reviews.scss',
-        'resources/sass/faq.scss',
-        'resources/sass/login.scss',
-        'resources/admin/style/admin.css',
-        'resources/admin/js/admin.js',
+        ...jsFiles,
+        ...cssFiles,
+        ...adminStyleScssFiles,
+        ...adminJsFiles,
       ],
       refresh: true,
     }),
