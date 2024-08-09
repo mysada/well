d3bee2bfd0761073af4294d26c87d73bb2c5e8eb
manish kumar <manishkumar@breezyloans.net.au>
2024-08-08 23:35:17 -0500

    Login. Register, Verify Page Style Changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,8 +13,9 @@
                         @csrf
 
                         <div class="mb-3">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             <label for="email" class="form-label">{{ __('Email address') }}</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
                             @error('email')
                             <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -23,8 +24,9 @@
                         </div>
 
                         <div class="mb-3">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             <label for="password" class="form-label">{{ __('Password') }}</label>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
                             @error('password')
                             <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
