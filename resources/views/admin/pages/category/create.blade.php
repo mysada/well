87773322c0c1600042abafb2016fa1d2a25d2269
dongqing <nights-1001@outlook.com>
2024-08-09 14:41:16 -0500

    created form

--- resources/views/admin/pages/category/create.blade.php
+++ resources/views/admin/pages/category/create.blade.php
@@ -4,5 +4,19 @@
 <div class="container mx-auto p-4">
     <h1 class="text-3xl font-bold mb-4">Create New Category</h1>
 
+    <!-- Form to create a new category -->
+    <form action="{{ route('AdminCategoryStore') }}" method="POST" enctype="multipart/form-data" class="space-y-4">
+        @csrf
+        <div class="form-control">
+            <label class="label" for="name">
+                <span class="label-text">Category Name</span>
+            </label>
+            <input type="text" name="name" id="name" class="input input-bordered w-full" required>
+        </div>
+
+        <div class="form-control">
+            <button type="submit" class="btn btn-primary">Create Category</button>
+        </div>
+    </form>
 </div>
 @endsection
