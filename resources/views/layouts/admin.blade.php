a660c5deb8f21b8aa75fdbbabf393e989e4edeae
Arthur NightFae <paracidex@gmail.com>
2024-08-08 23:01:34 -0500

    fix(UI): move admin scss to adminsass folder

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Dashboard')</title>
-    @vite('resources/sass/admin.scss')
+    @vite('resources/adminsass/admin.scss')
 </head>
 <body>
 <div id="app">
