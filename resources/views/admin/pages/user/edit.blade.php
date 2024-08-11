a3175a59e0a179f3b34b478bbb17389454f3cb8f
Aman Dawar <amandawar333@gmail.com>
2024-08-11 13:54:45 -0500

    aman - edit blade started

--- resources/views/admin/pages/user/edit.blade.php
+++ resources/views/admin/pages/user/edit.blade.php
@@ -1,1 +1,11 @@
-<?php
+@extends('admin.admin')
+
+@section('content')
+    <div class="container mx-auto">
+        <h2 class="text-2xl font-bold mb-4">Edit User</h2>
+
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+    @endif
