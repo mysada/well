32d15bfe678d61fa0c7065a73312fd80b21873bb
Aman Dawar <amandawar333@gmail.com>
2024-08-11 13:56:44 -0500

    aman - added a full name field

--- resources/views/admin/pages/user/edit.blade.php
+++ resources/views/admin/pages/user/edit.blade.php
@@ -9,3 +9,23 @@
                 {{ session('success') }}
             </div>
     @endif
+
+        <form action="{{ route('AdminUserUpdate', $user->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            <!-- Full Name -->
+            <div class="mb-4">
+                <label for="full_name" class="block text-sm font-medium">Full Name</label>
+                <input
+                    type="text"
+                    id="full_name"
+                    name="full_name"
+                    class="input input-bordered w-full"
+                    value="{{ old('full_name', $user->full_name) }}"
+                    required
+                >
+                @error('full_name')
+                <div class="text-red-600">{{ $message }}</div>
+                @enderror
+            </div>
