2e05d4dbd51b891bf9215a63138111a5b93ea1ea
dongqing <nights-1001@outlook.com>
2024-08-11 22:26:06 -0500

    css modified

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -1,8 +1,8 @@
-<div class="drawer-side" style="margin-right: 50px">
+<div class="drawer-side" style="padding-right: 50px; background-color: #66CC8A">
     <div class="menu text-base-content min-h-full w-40 p-4 gap-8">
         <div class="top-0 z-20 flex items-center gap-2 px-4 py-2">
             <a href="{{route('admin.home')}}">
-                <img src="/images/logo/header_logo.png" alt="Brand Logo" style="height: 40px;">
+                <img src="/images/logo/header_logo.png" alt="Brand Logo">
             </a>
         </div>
         <ul class="text-lg" style="font-size:15px">
