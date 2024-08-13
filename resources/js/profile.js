c5ea56908fdf1936403632adadca4b10dfa15fc1
manish kumar <manishkumar@breezyloans.net.au>
2024-08-13 11:42:42 -0500

    Flash Messages Timeout Function, CSS done

--- resources/js/profile.js
+++ resources/js/profile.js
@@ -1,13 +1,16 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-// Show the Order History section by default on page load
-document.getElementById('order-history').style.display = 'block';
+document.addEventListener("DOMContentLoaded", function () {
+    // Show the Order History section by default on page load
+    document.getElementById('order-history').style.display = 'block';
 
-document.querySelectorAll('.profile-link').forEach(link => {
-link.addEventListener('click', function() {
-document.querySelectorAll('.profile-section').forEach(section => section.style.display = 'none');
-const sectionId = this.getAttribute('data-section');
-document.getElementById(sectionId).style.display = 'block';
+    document.querySelectorAll('.profile-link').forEach(link => {
+        link.addEventListener('click', function () {
+            document.querySelectorAll('.profile-section').forEach(section => {
+                section.style.display = 'none';
+            });
+            const sectionId = this.getAttribute('data-section');
+            document.getElementById(sectionId).style.display = 'block';
+        });
+    });
 });
-});
-});
+
