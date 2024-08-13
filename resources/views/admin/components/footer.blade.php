de03e847e30afe155bdee99ac80825dd1da6a19d
Arthur NightFae <paracidex@gmail.com>
2024-08-12 20:41:25 -0500

    feat(admin): category edit and create ui

--- resources/views/admin/components/footer.blade.php
+++ resources/views/admin/components/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer items-center p-4 flex justify-between">
-        <img src="images/logo/header_logo.png" width="60px" alt="">
+        <img src="/images/logo/header_logo.png" width="60px" alt="">
         <p>Copyright © Well - All right reserved</p>
 </footer>
