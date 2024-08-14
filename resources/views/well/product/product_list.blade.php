3fe98afd1c66e9de9ba19a45abf1deb7fb99257d
dongqing <nights-1001@outlook.com>
2024-08-14 13:36:04 -0500

    category sidebar edited

--- resources/views/well/product/product_list.blade.php
+++ resources/views/well/product/product_list.blade.php
@@ -11,7 +11,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <div class="category-heading">Filter by Category</div>
+                <div class="category-heading">Category</div>
                 <div class="category-list">
                     @foreach ($categories as $category)
                     <a href="{{ route('products.index', ['category_id' => $category->id]) }}"
