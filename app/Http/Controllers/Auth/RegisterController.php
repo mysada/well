c3841a98aa09bb71c670e6c63a97246cdadd3ae8
Shivangikoradiya <shivangikoradiya9@gmail.com>
2024-08-14 10:36:43 -0500

    fixed bug for password validation on registration page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,16 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/[A-Z]/',      // must contain at least one uppercase letter
+                'regex:/[a-z]/',      // must contain at least one lowercase letter
+                'regex:/[0-9]/',      // must contain at least one number
+                'regex:/[@$!%*#?&]/', // must contain a special character
+                'confirmed'
+            ],
         ]);
     }
 
